45adf0198ac06faef6891620299b38e72df04d84
Modupe Akanni <modupe775@gmail.com>
2024-02-21 15:14:30 +0100

    add auth routes

--- backend/routes/auth/authIndex.js
+++ backend/routes/auth/authIndex.js
@@ -4,5 +4,12 @@ const express = require('express')
 const router = express.Router()
 const authController = require('../../controllers/authController')
 
+router.route('/register').post(authController.register)
+router.route('/login').post(authController.login)
+router.route('/logout').post(authController.logout)
+router.route('/login/auth/google').get(authController.googleAuth)
+router.route('/oauth2/redirect/google').get(authController.googleAuthRedirect)
+router.route('/forgotPassword').post(authController.forgotPassword)
+router.route('/resetPassword/:token').patch(authController.resetPassword)
 
 module.exports = router
