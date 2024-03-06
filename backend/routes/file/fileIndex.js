d9262c80eb116f5fdddd500fa782c557de16bf66
Modupe Akanni <modupe775@gmail.com>
2024-03-06 23:58:42 +0100

    feat: add delete route

--- backend/routes/file/fileIndex.js
+++ backend/routes/file/fileIndex.js
@@ -5,5 +5,6 @@ const fileController = require('../../controllers/fileController')
 router.route('/').get(fileController.getAllFiles)
 router.route('/:id').get(fileController.getById)
 router.route('/upload').post(fileController.uploadFile)
+router.route('/delete').post(fileController.deleteFile)
 
 module.exports = router
